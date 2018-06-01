221e6a51ae2aba3c1a2bc7c36d35b14c52d4d36b
Martina Lui <martina.lui@gmail.com>
2018-06-01 09:55:33 -0700

    Fix indentation of both programs.

--- power_int.ml
+++ power_int.ml
@@ -1,17 +1,19 @@
 (* power_int.ml Defines a function that take x and n as arguments to calculate the value of x to the power of n. x and n are of type int.*)
 
 let rec power_int x n =
-if n > 0 then
-match n with
-1 -> x
-| _ -> x * power_int x (n - 1)
-else
-match n with
-0 -> 1
-| _ -> (1 / x) * power_int x (n + 1) ;;
+  if n > 0 then
+    match n with
+      1 -> x
+      | _ -> x * power_int x (n - 1)
+  else
+    match n with
+      0 -> 1
+      | _ -> (1 / x) * power_int x (n + 1) ;;
+
 let main () =
   let arg1 = int_of_string Sys.argv.(1) and arg2 = int_of_string Sys.argv.(2) in
-  print_int (power_int arg1 arg2);
-  print_newline ();
-  exit 0;;
+    print_int (power_int arg1 arg2);
+    print_newline ();
+    exit 0;;
+
 main ();;
