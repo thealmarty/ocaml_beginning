73c09765c41b3ad06e966b20164e1b8c0f517450
Martina Lui <martina.lui@gmail.com>
2018-06-01 12:41:33 -0700

    Added the Fizzbuzz test program.

---
+++ fizzbuzz.ml
@@ -0,0 +1,23 @@
+(* fizzbuzz.ml Fizzbuzz test in OCaml*)
+
+let main x =
+  if x mod 3 = 0 then 
+    if x mod 5 = 0 then
+      print_string ("fizzbuzz")
+    else
+        print_string ("fizz")
+  else
+    if x mod 5 = 0 then
+      print_string ("buzz")
+    else
+      print_int x;
+  print_newline ();;
+ 
+let rec counter n =
+  match n with
+    1 -> main 1 
+    | _ -> counter (n - 1) ;
+           main n ;;
+           
+counter 100 ;;
+
