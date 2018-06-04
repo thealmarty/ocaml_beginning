4d865e78e6d18e5fa937fdeb887ee7c0ee4a92eb
Martina Lui <martina.lui@gmail.com>
2018-06-04 11:43:19 -0700

    Add comments explaining ; and ;;.

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -11,15 +11,15 @@ let fizzbuzztest x =
     if x mod 5 = 0 then
       print_string ("buzz")
     else
-      print_int x;
-  print_newline ();;
+      print_int x ; (*end of if statement*)
+  print_newline () ;; (*end of definition of fizzbuzztest *)
 
 (*count from n to 0 and print out the result of the function 'fizzbuzztest'*)
 let rec counter_fizzbuzz n =
   match n with
     0 -> () 
-    | _ -> counter_fizzbuzz (n - 1) ;
-           fizzbuzztest n ;;
+    | _ -> counter_fizzbuzz (n - 1) ; (*end of expression 1*)
+           fizzbuzztest n ;; (*end of definition of counter_fizzbuzz *)
            
-counter 100 ;;
+counter_fizzbuzz 100 ;;
 
